26e8b9eb13d0bed225fe9d41cec1b0f22874c48f
Dorn <dorn@savingcranes.org>
2020-02-03 16:13:44 -0600

    Partial changes - not getting all records

--- readEnrichment.py
+++ readEnrichment.py
@@ -33,69 +33,85 @@ __status__ = "ALpha"
 ### Processs Variables ###
 dataPath = "data"
 
-df = pd.read_excel(dataPath+"\ExampleEnrichment.xls",
-                   header=None, sheet_name="List")
-# print(df.head(10))
+outColumns = ['individual', 'localId', 'preferredID', 'species', 'birth_loc', 'birth_type', 'birthAge', 'current_collection', 'current_enclosure', 'enrichmentType', 'eCategory', 'eGoal', 'eDate', 'dateGiven', 'timeGiven', 'reaction', 'rating', 'providedBy', 'details']
 
-# Gather data to add to each row #
-individual = df.iat[0, 1]
-localID = df.iat[1, 1]
-preferredID = df.iat[2, 1]
-species = df.iat[3, 1]
-birth_loc = df.iat[4, 1]
-birth_type = df.iat[5, 1]
-birthAge = df.iat[6, 1]
-current_collection = df.iat[7, 1]
-current_enclosure = df.iat[8, 1]
-# print("\n")
-# print(len(df.index))
+outputData = pd.DataFrame(columns=outColumns)
 
-# Create DataFrame to store formatted data
-outData = []
-outColumns = ['individual', 'localId', 'preferredID', 'species', 'birth_loc', 'birth_type', 'birthAge', 'current_collection',
-              'current_enclosure', 'enrichmentType', 'eCategory', 'eGoal', 'eDate', 'dateGiven', 'timeGiven', 'reaction', 'rating', 'providedBy', 'details']
+for f in os.listdir(dataPath):
+    
+    if f.endswith(".xls") or f.endswith(".xlsx"):
+        # print(f)
 
-# Structure
-birdInfo = individual, localID, preferredID, species, birth_loc, birth_type, birthAge, current_collection, current_enclosure,
+        df = pd.read_excel(os.path.join(dataPath,f),
+                        header=None, sheet_name="List")
+        # print(df.head(10))
 
-#Begin searching for Enrichment #
-row = 9  # start where we left off
-enrichment = ""
-while row < len(df.index):
+        row = 0
+        birdInfoDict = {}
+        while pd.notnull(df.iat[row,1]):
+            birdInfoDict[df.iat[row,0].lower().replace(" ","").replace("/","_")]=df.iat[row,1]
+            # print(birdInfoDict)
+            row = row + 1
+            # # Gather data to add to each row #
+            # individual = df.iat[0, 1]
+            # localID = df.iat[1, 1]
+            # preferredID = df.iat[2, 1]
+            # species = df.iat[3, 1]
+            # birth_loc = df.iat[4, 1]
+            # birth_type = df.iat[5, 1]
+            # birthAge = df.iat[6, 1]
+            # current_collection = df.iat[7, 1]
+            # current_enclosure = df.iat[8, 1]
+            # # print("\n")
+            # # print(len(df.index))
+        # print(birdInfoDict)
 
-    if df.iat[row, 1] == "Enrichment Item Name":
-        row = row+1
-        enrichment = df.iat[row, 1]
-        eCategory = df.iat[row, 2]
-        eGoal = df.iat[row, 3]
-        eDate = df.iat[row, 4]
-        eInfo = enrichment, eCategory, eGoal, eDate
-        # print(birdInfo + eInfo)
+        # Create DataFrame to store formatted data
+        outData = []
 
-        # move to the records for this enrichment
-        row = row+3
-        while pd.notnull(df.iat[row, 1]):
-            # convert date
-            date = df.iat[row, 1]
+        # Structure
+        birdInfo = birdInfoDict['individual'], birdInfoDict['localid'], birdInfoDict['preferredid'], birdInfoDict['species'], birdInfoDict['birthlocation'], birdInfoDict['birthtype'], birdInfoDict['birth_age'], birdInfoDict['currentcollection'], birdInfoDict['currentenclosure']
 
-            time = df.iat[row, 2]
+        #Begin searching for Enrichment #
+        enrichment = ""
+        while row < len(df.index):
+            if df.iat[row, 1] == "Enrichment Item Name":
+                
+                row = row+1
+                enrichment = df.iat[row, 1]
+                eCategory = df.iat[row, 2]
+                eGoal = df.iat[row, 3]
+                eDate = df.iat[row, 4]
+                eInfo = enrichment, eCategory, eGoal, eDate
+                # print(birdInfo + eInfo)
 
-            # Handle nan values
-            reaction = df.iat[row, 3]
-            rating = df.iat[row, 4]
-            providedBy = df.iat[row, 5]
-            details = df.iat[row, 7]
+                # move to the records for this enrichment
+                row = row+3
+                while pd.notnull(df.iat[row, 1]):
+                    
+                    # convert date
+                    date = df.iat[row, 1]
+                    print(date)
 
-            actInfo = date, time, reaction, rating, providedBy, details
-            outData.append(birdInfo+eInfo+actInfo)
+                    time = df.iat[row, 2]
 
-            # print(str(date) + ", " + str(time) +
-            #       ", " + str(reaction) + ", " + str(details))
+                    # Handle nan values
+                    reaction = df.iat[row, 3]
+                    rating = df.iat[row, 4]
+                    providedBy = df.iat[row, 5]
+                    details = df.iat[row, 6]
+
+                    actInfo = date, time, reaction, rating, providedBy, details
+                    outData.append(birdInfo+eInfo+actInfo)
+
+                    # print(str(date) + ", " + str(time) +
+                    #       ", " + str(reaction) + ", " + str(details))
+
+                    row = row+1
+                enrichment = ""
 
             row = row+1
-        enrichment = ""
-
-    row = row+1
-    outDF = pd.DataFrame(outData, columns=outColumns)
-    print(outDF.head())
-    outDF.to_excel('output.xlsx')
+            outDF = pd.DataFrame(outData, columns=outColumns)
+            print(outDF.head())
+    outputData.append(outDF)
+outDF.to_excel('output.xlsx')
