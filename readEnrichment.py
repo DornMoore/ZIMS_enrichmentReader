153d96e0f87ae8672e399be064fd7027fd5c9801
Dorn Moore <dorn@savingcranes.org>
2020-02-04 08:17:32 -0600

    Fixed Output to place all records in output xlsx file.

--- readEnrichment.py
+++ readEnrichment.py
@@ -33,50 +33,41 @@ __status__ = "ALpha"
 ### Processs Variables ###
 dataPath = "data"
 
-outColumns = ['individual', 'localId', 'preferredID', 'species', 'birth_loc', 'birth_type', 'birthAge', 'current_collection', 'current_enclosure', 'enrichmentType', 'eCategory', 'eGoal', 'eDate', 'dateGiven', 'timeGiven', 'reaction', 'rating', 'providedBy', 'details']
+outColumns = ['individual', 'localId', 'preferredID', 'species', 'birth_loc', 'birth_type', 'birthAge', 'current_collection',
+              'current_enclosure', 'enrichmentType', 'eCategory', 'eGoal', 'eDate', 'dateGiven', 'timeGiven', 'reaction', 'rating', 'providedBy', 'details']
 
-outputData = pd.DataFrame(columns=outColumns)
+
+outData = []
 
 for f in os.listdir(dataPath):
-    
+
     if f.endswith(".xls") or f.endswith(".xlsx"):
         # print(f)
 
-        df = pd.read_excel(os.path.join(dataPath,f),
-                        header=None, sheet_name="List")
+        df = pd.read_excel(os.path.join(dataPath, f),
+                           header=None, sheet_name="List")
         # print(df.head(10))
 
         row = 0
         birdInfoDict = {}
-        while pd.notnull(df.iat[row,1]):
-            birdInfoDict[df.iat[row,0].lower().replace(" ","").replace("/","_")]=df.iat[row,1]
+        while pd.notnull(df.iat[row, 1]):
+            key = df.iat[row, 0].lower()  # set the key to the field lowercase
+            key = key.replace(" ", "")      # remove any spaces
+            key = key.replace("/", "_")      # Change / to _
+
+            birdInfoDict[key] = df.iat[row, 1]
             # print(birdInfoDict)
             row = row + 1
-            # # Gather data to add to each row #
-            # individual = df.iat[0, 1]
-            # localID = df.iat[1, 1]
-            # preferredID = df.iat[2, 1]
-            # species = df.iat[3, 1]
-            # birth_loc = df.iat[4, 1]
-            # birth_type = df.iat[5, 1]
-            # birthAge = df.iat[6, 1]
-            # current_collection = df.iat[7, 1]
-            # current_enclosure = df.iat[8, 1]
-            # # print("\n")
-            # # print(len(df.index))
-        # print(birdInfoDict)
 
-        # Create DataFrame to store formatted data
-        outData = []
-
-        # Structure
-        birdInfo = birdInfoDict['individual'], birdInfoDict['localid'], birdInfoDict['preferredid'], birdInfoDict['species'], birdInfoDict['birthlocation'], birdInfoDict['birthtype'], birdInfoDict['birth_age'], birdInfoDict['currentcollection'], birdInfoDict['currentenclosure']
+        # Structure - this needs to be re-configured to be more flexible
+        birdInfo = birdInfoDict['individual'], birdInfoDict['localid'], birdInfoDict['preferredid'], birdInfoDict['species'], birdInfoDict[
+            'birthlocation'], birdInfoDict['birthtype'], birdInfoDict['birth_age'], birdInfoDict['currentcollection'], birdInfoDict['currentenclosure']
 
         #Begin searching for Enrichment #
         enrichment = ""
         while row < len(df.index):
             if df.iat[row, 1] == "Enrichment Item Name":
-                
+
                 row = row+1
                 enrichment = df.iat[row, 1]
                 eCategory = df.iat[row, 2]
@@ -88,10 +79,10 @@ for f in os.listdir(dataPath):
                 # move to the records for this enrichment
                 row = row+3
                 while pd.notnull(df.iat[row, 1]):
-                    
+
                     # convert date
                     date = df.iat[row, 1]
-                    print(date)
+                    # print(date)
 
                     time = df.iat[row, 2]
 
@@ -102,16 +93,14 @@ for f in os.listdir(dataPath):
                     details = df.iat[row, 6]
 
                     actInfo = date, time, reaction, rating, providedBy, details
-                    outData.append(birdInfo+eInfo+actInfo)
 
-                    # print(str(date) + ", " + str(time) +
-                    #       ", " + str(reaction) + ", " + str(details))
+                    # Add Row of Data to outData
+                    outData.append(birdInfo+eInfo+actInfo)
 
                     row = row+1
                 enrichment = ""
 
             row = row+1
-            outDF = pd.DataFrame(outData, columns=outColumns)
-            print(outDF.head())
-    outputData.append(outDF)
-outDF.to_excel('output.xlsx')
+
+export = pd.DataFrame(outData, columns=outColumns)
+export.to_excel('output.xlsx')
